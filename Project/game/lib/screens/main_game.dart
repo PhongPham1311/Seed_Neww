ad6b17e5e2160d62d203288efd31d5c9364c243b
unknown <0306201271@caothang.edu.vn>
2022-11-11 15:05:18 +0700

    them giao dien thay doi anh dai dien

--- Project/game/lib/screens/main_game.dart
+++ Project/game/lib/screens/main_game.dart
@@ -882,3 +882,188 @@ _ontapviewprofile(BuildContext context) {
     ),
   );
 }
+_ontapchangeavatar(BuildContext context) {
+  return Material(
+    color: const Color(0x00000000),
+    child: Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        Positioned(
+          top: 200.0,
+          child: Container(
+              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+              height: 400,
+              width: 340,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('img/frame2.png'), fit: BoxFit.fill)),
+              child: Container(
+                margin: EdgeInsets.fromLTRB(15, 40, 20, 80),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          width: 80,
+                          height: 80,
+                          decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('img/frame3.png'),
+                                  fit: BoxFit.fill)),
+                          child: Image.asset(
+                            'img/avatar.jpg',
+                            fit: BoxFit.fill,
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      height: 180,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(8),
+                                width: 80,
+                                height: 80,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage('img/frame3.png'),
+                                        fit: BoxFit.fill)),
+                                child: Image.asset(
+                                  'img/avatar.jpg',
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(8),
+                                width: 80,
+                                height: 80,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage('img/frame3.png'),
+                                        fit: BoxFit.fill)),
+                                child: Image.asset(
+                                  'img/avatar.jpg',
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(8),
+                                width: 80,
+                                height: 80,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage('img/frame3.png'),
+                                        fit: BoxFit.fill)),
+                                child: Image.asset(
+                                  'img/avatar.jpg',
+                                  fit: BoxFit.fill,
+                                ),
+                              )
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(8),
+                                width: 80,
+                                height: 80,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage('img/frame3.png'),
+                                        fit: BoxFit.fill)),
+                                child: Image.asset(
+                                  'img/avatar.jpg',
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(8),
+                                width: 80,
+                                height: 80,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage('img/frame3.png'),
+                                        fit: BoxFit.fill)),
+                                child: Image.asset(
+                                  'img/avatar.jpg',
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(8),
+                                width: 80,
+                                height: 80,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage('img/frame3.png'),
+                                        fit: BoxFit.fill)),
+                                child: Image.asset(
+                                  'img/avatar.jpg',
+                                  fit: BoxFit.fill,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+        ),
+        Positioned(
+            top: 190,
+            right: 5,
+            child: InkWell(
+              child: Image.asset(
+                'img/cancelbutton.png',
+                width: 50,
+                height: 50,
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            )),
+        Positioned(
+          top: 565,
+          child: Container(
+            width: 100,
+            height: 50,
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('img/button1.png'), fit: BoxFit.fill)),
+            child: InkWell(
+              child: const Text(
+                'Lưu',
+                style: TextStyle(
+                    shadows: [
+                      Shadow(
+                        offset: Offset(5, 3),
+                        blurRadius: 10,
+                        color: Colors.black,
+                      ),
+                    ],
+                    color: Colors.white,
+                    fontFamily: 'Mono',
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800),
+              ),
+              onTap: () {},
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
