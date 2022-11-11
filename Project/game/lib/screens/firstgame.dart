d68bce4236d9bf03c3ef897bd5333ab440d816fd
tuthanh299 <0306201299@caothang.edu.vn>
2022-11-11 16:12:49 +0700

    cap nhat lai giao dien doi ten

--- Project/game/lib/screens/firstgame.dart
+++ Project/game/lib/screens/firstgame.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:seed_new/screens/main_game.dart';
+import 'main_game.dart';
+
 class Firstgame extends StatefulWidget {
   const Firstgame({super.key});
 
@@ -105,7 +106,8 @@ class _FirstgameState extends State<Firstgame> {
                             width: 150,
                             height: 50,
                             child: Stack(children: [
-                              Positioned(left: 20,
+                              Positioned(
+                                left: 20,
                                 child: Image.asset(
                                   'img/logintoptb.png',
                                   height: 70,
@@ -139,7 +141,7 @@ class _FirstgameState extends State<Firstgame> {
                         right: 0.0,
                         left: 0.0,
                         child: Container(
-                          margin: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                            margin: const EdgeInsets.fromLTRB(15, 0, 15, 0),
                             alignment: Alignment.center,
                             height: 100,
                             decoration: const BoxDecoration(
@@ -256,8 +258,10 @@ class _FirstgameState extends State<Firstgame> {
                             ),
                           ),
                           onTap: () {
-                            Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const MainGame()));
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const MainGame()));
                           },
                         ),
                       )
