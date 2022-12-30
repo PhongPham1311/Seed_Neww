aef77ae968f97f2c24c2bd707a11a20115929faa
unknown <0306201271@caothang.edu.vn>
2022-12-30 15:41:56 +0700

    update_main3

--- Project/game/lib/main.dart
+++ Project/game/lib/main.dart
@@ -27,3 +27,161 @@ class MyApp extends StatelessWidget {
     return MyHomePage();
   }
 }
+lass MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String id = "";
+  String is_logined = "";
+
+  get_is_logined() async {
+    final SharedPreferences cookie = await SharedPreferences.getInstance();
+    is_logined = cookie.getString('is_logined') != null
+        ? cookie.getString('is_logined')!
+        : '';
+    setState(() {});
+  }
+
+  get_id() async {
+    final SharedPreferences cookie = await SharedPreferences.getInstance();
+    id = cookie.getString('id') != null ? cookie.getString('id')! : '';
+    setState(() {});
+  }
+
+  final player = AudioPlayer();
+  void playLocal() async {
+    await player.play(AssetSource("audio/QuizMusic.mp3"));
+  }
+
+  void loop() {
+    player.setReleaseMode(ReleaseMode.loop);
+  }
+
+  @override
+  void initState() {
+    playLocal();
+    loop();
+    get_id();
+    get_is_logined();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (is_logined == "1" && id != "") {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MainGame(),
+      );
+    } else {
+      return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MainForm(),
+      );
+    }
+  }
+}
+
+class MainForm extends StatefulWidget {
+  const MainForm({super.key});
+
+  @override
+  State<MainForm> createState() => _MainFormState();
+}
+
+class _MainFormState extends State<MainForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      padding: const EdgeInsets.only(top: 30),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('img/background.png'), fit: BoxFit.fill),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset(
+            'img/logo.png',
+            width: 200,
+            height: 200,
+          ),
+          Column(
+            children: [
+              InkWell(
+                child: Container(
+                  margin: const EdgeInsets.fromLTRB(0, 100, 0, 20),
+                  width: 150,
+                  height: 50,
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('img/table.png'),
+                          fit: BoxFit.fill)),
+                  child: const Text(
+                    'Đăng Nhập',
+                    style: TextStyle(
+                        shadows: [
+                          Shadow(
+                            offset: Offset(5, 3),
+                            blurRadius: 10,
+                            color: Colors.black,
+                          ),
+                        ],
+                        color: Colors.white,
+                        fontFamily: 'Mono',
+                        fontSize: 20,
+                        fontWeight: FontWeight.w800),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Login()));
+                },
+              ),
+              InkWell(
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('img/table.png'),
+                          fit: BoxFit.fill)),
+                  child: const Text(
+                    'Đăng Ký',
+                    style: TextStyle(
+                        shadows: [
+                          Shadow(
+                            offset: Offset(5, 3),
+                            blurRadius: 10,
+                            color: Colors.black,
+                          ),
+                        ],
+                        color: Colors.white,
+                        fontFamily: 'Mono',
+                        fontSize: 20,
+                        fontWeight: FontWeight.w800),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Register()));
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
+  }
+}
