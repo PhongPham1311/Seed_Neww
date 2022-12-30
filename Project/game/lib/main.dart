c5fad606daf351b409cd8b7cccb6f98be3b6ac1b
unknown <0306201271@caothang.edu.vn>
2022-12-30 15:39:25 +0700

    update_main

--- Project/game/lib/main.dart
+++ Project/game/lib/main.dart
@@ -1,123 +1,21 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:date_field/date_field.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'screens/login.dart';
-import 'screens/register.dart';
 import 'package:flutter/services.dart';
+import 'main_game.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'firstgame.dart';
+import 'login.dart';
+import 'register.dart';
 
-void main() {
+Future<void> main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIOverlays([
     SystemUiOverlay.bottom, //This line is used for showing the bottom bar
   ]);
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      margin: const EdgeInsets.only(top: 30),
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage('img/background.png'), fit: BoxFit.fill),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Image.asset(
-            'img/logo.png',
-            width: 200,
-            height: 200,
-          ),
-          Column(
-            children: [
-              InkWell(
-                child: Container(
-                  margin: const EdgeInsets.fromLTRB(0, 100, 0, 20),
-                  width: 150,
-                  height: 50,
-                  alignment: Alignment.center,
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('img/table.png'),
-                          fit: BoxFit.fill)),
-                  child: const Text(
-                    'Đăng Nhập',
-                    style: TextStyle(
-                        shadows: [
-                          Shadow(
-                            offset: Offset(5, 3),
-                            blurRadius: 10,
-                            color: Colors.black,
-                          ),
-                        ],
-                        color: Colors.white,
-                        fontFamily: 'Mono',
-                        fontSize: 20,
-                        fontWeight: FontWeight.w800),
-                  ),
-                ),
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const Login()));
-                },
-              ),
-              InkWell(
-                child: Container(
-                  width: 150,
-                  height: 50,
-                  alignment: Alignment.center,
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('img/table.png'),
-                          fit: BoxFit.fill)),
-                  child: const Text(
-                    'Đăng Ký',
-                    style: TextStyle(
-                        shadows: [
-                          Shadow(
-                            offset: Offset(5, 3),
-                            blurRadius: 10,
-                            color: Colors.black,
-                          ),
-                        ],
-                        color: Colors.white,
-                        fontFamily: 'Mono',
-                        fontSize: 20,
-                        fontWeight: FontWeight.w800),
-                  ),
-                ),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const Register()));
-                },
-              ),
-            ],
-          ),
-        ],
-      ),
-    ));
-  }
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
